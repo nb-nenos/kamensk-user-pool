87b0e5ed785e2deb347c4437de089f860fbc47c9
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-10-15 11:09:42 -0300

    initial commit

---
+++ src/main/kotlin/com/kamenskuserpool/KamenskUserPoolApplication.kt
@@ -0,0 +1,11 @@
+package com.kamenskuserpool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KamenskUserPoolApplication
+
+fun main(args: Array<String>) {
+    runApplication<KamenskUserPoolApplication>(*args)
+}
