9ae3a829bb9dd75c166d241f597b640215cdc2cf
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-10-22 12:05:02 -0300

    feat: info properties

---
+++ src/main/kotlin/com/kamenskuserpool/configs/properties/InfoProperties.kt
@@ -0,0 +1,14 @@
+package com.kamenskuserpool.configs.properties
+
+import com.kamenskuserpool.enums.UserTypeEnum
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("kamensk-user-pool.info")
+data class InfoProperties(
+    val fullName: String,
+    val dateOfBirth: String,
+    val email: String,
+    val password: String,
+    val motherName: String,
+    val safetyPhrase: String
+)
