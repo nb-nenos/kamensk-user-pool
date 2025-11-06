f9d287d8deb72a4e474e790182db5350e4061594
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-11-06 17:23:58 -0300

    config

--- src/main/kotlin/com/kamenskuserpool/KamenskUserPoolApplication.kt
+++ src/main/kotlin/com/kamenskuserpool/KamenskUserPoolApplication.kt
@@ -2,8 +2,12 @@ package com.kamenskuserpool
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
 class KamenskUserPoolApplication
 
 fun main(args: Array<String>) {
