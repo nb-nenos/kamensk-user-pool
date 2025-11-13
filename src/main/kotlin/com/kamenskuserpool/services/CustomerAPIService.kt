04f96075d8ed71fcd1defe675a7e1eaebadf3efe
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-11-13 14:46:40 -0300

    KSK-2807: changed class name

--- src/main/kotlin/com/kamenskuserpool/services/CustomerAPIService.kt
+++ src/main/kotlin/com/kamenskuserpool/services/CustomerAPIService.kt
@@ -1,11 +1,9 @@
 package com.kamenskuserpool.services
 
 import com.kamenskuserpool.clients.CustomerAPIClient
-import com.kamenskuserpool.dtos.ResponseUserDto
+import com.kamenskuserpool.dtos.ResponseCustomerAPIDto
 import com.kamenskuserpool.dtos.UserDto
 import com.kamenskuserpool.exceptions.ClientException
-import com.kamenskuserpool.exceptions.SafepayUserIdException
-import feign.FeignException
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -15,7 +13,7 @@ class CustomerAPIService(
 ) {
     private val logger = LoggerFactory.getLogger(UserService::class.java)
 
-    fun createUser(userDto: UserDto): ResponseUserDto{
+    fun createUser(userDto: UserDto): ResponseCustomerAPIDto{
         return runCatching {
             customerAPIClient.createUser(userDto)
         }.onFailure {
