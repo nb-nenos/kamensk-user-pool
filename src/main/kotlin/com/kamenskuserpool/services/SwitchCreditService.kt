736d81fa6162c0cc1e506540a99c83d1bfeefcec
Bruna Suzuki <brunasuzuki04@gmail.com>
2026-01-01 16:12:49 -0300

    KSK-2807: credit service

--- src/main/kotlin/com/kamenskuserpool/services/SwitchCreditService.kt
+++ src/main/kotlin/com/kamenskuserpool/services/SwitchCreditService.kt
@@ -3,27 +3,26 @@ package com.kamenskuserpool.services
 import com.kamenskuserpool.dtos.RequestSwitchCreditDto
 import com.kamenskuserpool.exceptions.SwitchFlagException
 import com.kamenskuserpool.exceptions.UserNotFoundException
-import com.kamenskuserpool.repositories.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class SwitchCreditService(
-    private val userRepository: UserRepository
+    private val userService: UserService
 ) {
 
     fun switchCredit(switchCreditPayload: RequestSwitchCreditDto): String {
-        val creditCustomer = userRepository.findByCustomerId(switchCreditPayload.customerId)
+        val creditCustomer = userService.findByCustomerId(switchCreditPayload.customerId)
             ?: throw UserNotFoundException()
 
         when (switchCreditPayload.switchCredit) {
             "on" -> {
                 creditCustomer.creditFlg = true
-                userRepository.save(creditCustomer)
+                userService.save(creditCustomer)
                 return "Credit On"
             }
             "off" -> {
                 creditCustomer.creditFlg = false
-                userRepository.save(creditCustomer)
+                userService.save(creditCustomer)
                 return "Credit Off"
             }
             else -> {
